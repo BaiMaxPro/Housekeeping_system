788afcd8e565e81e3849a805f34133c9cd2891ee
Chen Xinyang <chenxy1999728@126.com>
2020-09-10 01:51:08 +0800

    [Backend-Employee] Added views to main app

--- backend/app.py
+++ backend/app.py
@@ -28,6 +28,11 @@ api.add_resource(CustomerRootAPI, "/customer")
 api.add_resource(CustomerAPI, "/customer/<string:id>")
 api.add_resource(CustomerOrdersAPI, "/customer/<string:id>/orders")
 
+from backend.employee.view import EmployeeRootAPI, EmployeeAPI, EmployeeOrdersAPI
+api.add_resource(EmployeeRootAPI, "/employee")
+api.add_resource(EmployeeAPI, "/employee/<string:id>")
+api.add_resource(EmployeeOrdersAPI, "/employee/<string:id>/orders")
+
 app.register_blueprint(blueprint, url_prefix='/api')
 
 if __name__ == '__main__':
