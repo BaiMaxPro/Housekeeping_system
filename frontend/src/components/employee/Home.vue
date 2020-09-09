bd6f7b90742d5106c8354d0bca0293a02ba1c351
BaiMaxPro <fmb@smartbai.cn>
2020-09-10 03:24:36 +0800

    将customer作为模板添加employee页面

---
+++ frontend/src/components/employee/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-card class="mx-auto" max-width="420">
+        <h1 class="mx-10" align="center">欢迎来到雇员主页</h1>
+        <v-img height = "1080/2" width="1920/2" src="../../images/customer2.jpg"><p>Something else here</p></v-img>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
