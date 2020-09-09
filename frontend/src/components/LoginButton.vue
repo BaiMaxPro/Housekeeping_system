fbfd9b84305baf37d43299608ca2f2944029eb7b
Chen Xinyang <chenxy1999728@126.com>
2020-09-10 02:45:30 +0800

    [Frontend-Nav] Added functionality for login button

--- frontend/src/components/LoginButton.vue
+++ frontend/src/components/LoginButton.vue
@@ -4,10 +4,23 @@
         <v-icon>mdi-account</v-icon>
         <span>登录</span>
       </v-btn>
-      <v-btn outlined v-else>
-        <v-icon>mdi-account</v-icon>
-        <span>{{ user.username }} 已登录</span>
-      </v-btn>
+      <v-menu open-on-hover v-else offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn outlined v-bind="attrs" v-on="on">
+            <v-icon>mdi-account</v-icon>
+            <span>{{ user.username }} 已登录</span>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item :to="userHome.link">
+            <v-list-item-title>{{ userHome.name }}</v-list-item-title>
+          </v-list-item>
+          <v-list-item v-on:click="logout">
+            <v-list-item-title>退出登录</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
   </span>
 </template>
 
@@ -19,11 +32,22 @@ export default {
   
   computed: {
     ...mapGetters(['loggedIn', 'user']),
+    userHome: function(){
+      if(this.user.role == "customer") return {name: "顾客中心", link: "/customer/home"};
+      if(this.user.role == "employee") return {name: "员工中心", link: "/employee/home"};
+      return {name: "用户中心", link: "/"};
+    },
   },
 
   mounted() {
     this.$store.dispatch('recoverSession');
   },
+
+  methods: {
+    logout: async function(){
+      await this.$store.dispatch('logout');
+    },
+  }
 
 }
 </script>
