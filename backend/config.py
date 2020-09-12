8d0d893ea123cf49df438e88764eaadb6dcdebf4
Chen Xinyang <chenxy1999728@126.com>
2020-09-12 23:03:58 +0800

    [Backend-Config] Added DB init configs & flask_env

--- backend/config.py
+++ backend/config.py
@@ -8,3 +8,9 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 DB_URI = os.getenv("SQLALCHEMY_DATABASE_URI") or "sqlite:///"
 PORT = os.getenv("PORT") or 5000
+
+FLASK_ENV = os.getenv("FLASK_ENV") or "production"
+
+DEBUG = FLASK_ENV == "debug"
+
+INIT_DATABASE = os.getenv("FLASK_INIT_DATABASE") == 'true'
